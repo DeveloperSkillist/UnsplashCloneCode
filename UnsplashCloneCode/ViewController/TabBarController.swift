12751af61e5b894f7052d745dc92e024054734ee
skillist <srpgs2@gmail.com>
2021-11-28 14:22:38 +0900

    tabbar 컬러 변경

--- UnsplashCloneCode/ViewController/TabBarController.swift
+++ UnsplashCloneCode/ViewController/TabBarController.swift
@@ -12,12 +12,25 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTabbarLayout()
+        setupTabbarItem()
+    }
+    
+    private func setupTabbarLayout() {
+        //tabbar 아이템의 틴트 컬러 변경
+        tabBar.tintColor = .white
+        
+        //tabbar 컬러 변경
+        tabBar.barTintColor = .black
+    }
+    
+    private func setupTabbarItem() {
         //TabBar의 아이템 설정
         
         //photo Tab
         let photoListViewController = PhotoListViewController()
         photoListViewController.tabBarItem = UITabBarItem(
-            title: "Photos",
+            title: "Photo",
             image: UIImage(systemName: "photo"),
             selectedImage: UIImage(systemName: "photo.fill")
         )
