ed4dc83c588d820c762533f45b360b69be678e21
skillist <srpgs2@gmail.com>
2021-12-05 01:28:57 +0900

    search result를 searchResultsController로 구현 중

--- UnsplashCloneCode/ViewController/Search/SearchMain/SearchMainViewController.swift
+++ UnsplashCloneCode/ViewController/Search/SearchMain/SearchMainViewController.swift
@@ -9,25 +9,11 @@ import UIKit
 import SnapKit
 
 class SearchMainViewController: UICollectionViewController {
-    private var searchText = ""
-    private let searchResultCollectionViewTag = 1
-    private var searchPageNum = 0
-    private lazy var searchResultCollectionView: UICollectionView = {
-//        let layout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout22())
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.tag = searchResultCollectionViewTag
-        
-//        collectionView.prefetchDataSource = self
-        collectionView.register(PhotoListCollectionViewCell.self, forCellWithReuseIdentifier: "PhotoListCollectionViewCell")
-        collectionView.backgroundColor = .red
-        collectionView.isHidden = true
-        collectionView.keyboardDismissMode = .onDrag
-        return collectionView
+    private lazy var searchResultVC: SearchResultCollectionViewController = {
+       var searchResultVC = SearchResultCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        searchResultVC.collectionView.delegate = self
+        return searchResultVC
     }()
-    private var isSearchFetching = false
-    var searchItems: [Any] = []
     
     private var discoverPageNum = 0
     private var contents: [SearchMainItem] = [
@@ -37,11 +23,12 @@ class SearchMainViewController: UICollectionViewController {
     private var isFetching = false
     
     private lazy var searchController: UISearchController = {
-        let searchController = UISearchController(searchResultsController: nil)
+        let searchController = UISearchController(searchResultsController: searchResultVC)
 //        searchController.delegate = self
 //        searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
         searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchResultsUpdater = self
         definesPresentationContext = true
         
         searchController.searchBar.delegate = self
@@ -74,7 +61,7 @@ class SearchMainViewController: UICollectionViewController {
     }
     
     private func setupCollectionView() {
-        collectionView.keyboardDismissMode = .onDrag
+//        collectionView.keyboardDismissMode = .onDrag
         collectionView.backgroundColor = .black
         collectionView.delegate = self
         
@@ -94,11 +81,6 @@ class SearchMainViewController: UICollectionViewController {
     private func setupLayout() {
         view.backgroundColor = .black
         collectionView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        
-        view.addSubview(searchResultCollectionView)
-        (searchResultCollectionView).snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
@@ -190,61 +172,6 @@ extension SearchMainViewController {
             }
         }
     }
-    
-    func fetchSearch() {
-        if isSearchFetching {
-            return
-        }
-        isSearchFetching = true
-        
-        let searchTypeNum = navigationItem.searchController?.searchBar.selectedScopeButtonIndex ?? 0
-        var searchTypeString = navigationItem.searchController?.searchBar.scopeButtonTitles?[searchTypeNum] ?? "photos"
-        searchTypeString = searchTypeString.lowercased()
-        
-        UnsplashAPI.fetchSearchResult(searchText: searchText, searchType: SearchType(rawValue: searchTypeString) ?? SearchType.Photos, pageNum: searchPageNum + 1) { [weak self] data, response, error in
-            self?.isSearchFetching = false
-            guard error == nil,
-                  let response = response as? HTTPURLResponse,
-                  let data = data else {
-                      DispatchQueue.main.async {    //에러 발생 시 에러 보여주기
-                          self?.showNetworkErrorAlert(error: .networkError)
-                      }
-                      return
-                  }
-            
-            switch response.statusCode {
-            //response 성공 시, 목록 설정하기
-            case (200...299):
-                do {
-//                    print("data : \(data.description)")
-                    let result = try JSONDecoder().decode(SearchPhotos.self, from: data)
-                    
-                    if self?.searchPageNum == 0 { //첫페이지를 가져온 경우 목록 설정
-                        self?.searchItems = result.results
-                    } else { //첫페이지 외 다음페이지를 가져온 경우 목록 설정
-                        self?.searchItems.append(contentsOf: result.results)
-                    }
-                    print("itemSize: \(self?.searchItems.count)")
-                    
-                    DispatchQueue.main.async {
-                        //다음 페이지 번호 설정
-                        self?.searchPageNum += 1
-                        self?.searchResultCollectionView.reloadData()
-                    }
-                } catch {
-                    DispatchQueue.main.async {  //에러 발생 시 에러 보여주기
-                        self?.showNetworkErrorAlert(error: .jsonParsingError)
-                    }
-                }
-                
-            default:
-                DispatchQueue.main.async {  //에러 발생 시 에러 보여주기
-                    self?.showNetworkErrorAlert(error: .networkError)
-                }
-                return
-            }
-        }
-    }
 }
 
 extension SearchMainViewController {
@@ -321,117 +248,70 @@ extension SearchMainViewController {
 extension SearchMainViewController {
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        switch collectionView {
-        case self.collectionView :
-            return contents.count
-        case searchResultCollectionView:
-            return 1
-        default:
-            return 0
-        }
+        return contents.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch collectionView {
-        case self.collectionView :
-            return contents[section].items.count
-        case searchResultCollectionView:
-            print("searchItems : \(searchItems.count)")
-            return searchItems.count
-        default:
-            return 0
-        }
+        return contents[section].items.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch collectionView {
-        case self.collectionView :
-            switch contents[indexPath.section].type {
-            case .category:
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCategoryCollectionViewCell", for: indexPath) as? SearchCategoryCollectionViewCell else {
-                    return UICollectionViewCell()
-                }
-                
-                guard let categories = contents[indexPath.section].items as? [Category] else {
-                    return UICollectionViewCell()
-                }
-                let category = categories[indexPath.row]
-                cell.setup(category: category)
-                return cell
-                
-            case .discover:
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchDiscoverCollectionViewCell", for: indexPath) as? SearchDiscoverCollectionViewCell else {
-                    return UICollectionViewCell()
-                }
-                
-                guard let photos = contents[indexPath.section].items as? [Photo] else {
-                    return UICollectionViewCell()
-                }
-                let photo = photos[indexPath.row]
-                cell.setup(photo: photo)
-                return cell
-            }
-        case searchResultCollectionView:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoListCollectionViewCell", for: indexPath) as? PhotoListCollectionViewCell else {
+        
+        switch contents[indexPath.section].type {
+        case .category:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCategoryCollectionViewCell", for: indexPath) as? SearchCategoryCollectionViewCell else {
                 return UICollectionViewCell()
             }
-            guard let item = searchItems[indexPath.row] as? Photo else {
+            
+            guard let categories = contents[indexPath.section].items as? [Category] else {
                 return UICollectionViewCell()
             }
-            cell.setup(photo: item)
+            let category = categories[indexPath.row]
+            cell.setup(category: category)
             return cell
-        default:
-            return UICollectionViewCell()
+            
+        case .discover:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchDiscoverCollectionViewCell", for: indexPath) as? SearchDiscoverCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            guard let photos = contents[indexPath.section].items as? [Photo] else {
+                return UICollectionViewCell()
+            }
+            let photo = photos[indexPath.row]
+            cell.setup(photo: photo)
+            return cell
         }
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch collectionView {
-        case self.collectionView :
-            if kind == UICollectionView.elementKindSectionHeader {
-                guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SearchMainCollectionViewHeader", for: indexPath) as? SearchMainCollectionViewHeader else {
-                    return UICollectionReusableView()
-                }
-                
-                switch contents[indexPath.section].type {
-                case .category:
-                    headerView.headerLabel.text = "Browse by Category"
-                case .discover:
-                    headerView.headerLabel.text = "Discover"
-                }
-                return headerView
-            } else {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SearchMainCollectionViewHeader", for: indexPath) as? SearchMainCollectionViewHeader else {
                 return UICollectionReusableView()
             }
-        case searchResultCollectionView:
-            return UICollectionReusableView()
-        default:
+            
+            switch contents[indexPath.section].type {
+            case .category:
+                headerView.headerLabel.text = "Browse by Category"
+            case .discover:
+                headerView.headerLabel.text = "Discover"
+            }
+            return headerView
+        } else {
             return UICollectionReusableView()
         }
     }
     
     override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        switch collectionView {
-        case self.collectionView :
-            if indexPath.section != 1 {
-                return
-            }
-            
-            let row = indexPath.row
-            if row == contents[1].items.count - 11 ||
-                row == contents[1].items.count - 6 ||
-                row == contents[1].items.count - 1 {
-                fetchDiscover()
-            }
-        case searchResultCollectionView:
-            let row = indexPath.row
-            if row == searchItems.count - 11 ||
-                row == searchItems.count - 6 ||
-                row == searchItems.count - 1 {
-                fetchSearch()
-            }
-        default:
+        if indexPath.section != 1 {
             return
+        }
+        
+        let row = indexPath.row
+        if row == contents[1].items.count - 11 ||
+            row == contents[1].items.count - 6 ||
+            row == contents[1].items.count - 1 {
+            fetchDiscover()
         }
     }
 }
@@ -453,64 +333,41 @@ extension SearchMainViewController: UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchText = searchBar.text ?? ""
-        if !searchText.isEmpty {
+        let searchBarText = searchBar.text ?? ""
+        if !searchBarText.isEmpty {
             searchBar.showsScopeBar = true
-            searchResultCollectionView.isHidden = false
             
             view.endEditing(true)
             //search
-            searchPageNum = 0
-            fetchSearch()
+            searchResultVC.fetchFirstSearch(searchText: searchBarText)
         }
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.showsCancelButton = false
         searchBar.showsScopeBar = false
-        searchResultCollectionView.isHidden = true
+        
+        searchResultVC.resetResult()
     }
     
     func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        var selectType: SearchType = .Photos
+        switch selectedScope {
+        case 1:
+            selectType = .Collections
+        case 2:
+            selectType = .Users
+        default:
+            selectType = .Photos
+        }
+        
         //search
-        searchPageNum = 0
-        fetchSearch()
+        searchResultVC.currentSearchType = selectType
     }
 }
 
-extension SearchMainViewController {
-    private func layout22() -> UICollectionViewLayout {
-        return UICollectionViewCompositionalLayout { [weak self] section, _ -> NSCollectionLayoutSection? in
-//            switch self?.contents[section].type {
-//            case .category:
-//                return self?.createCategorySection()
-//            case .discover:
-//
-//            default:
-//                return nil
-//            }
-            return self?.createPhotoSection()
-        }
-    }
-    
-    //TODO discover
-    private func createPhotoSection() -> NSCollectionLayoutSection {
-        //아이템
-        let itemMargin: CGFloat = 1
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = .init(top: itemMargin, leading: itemMargin, bottom: itemMargin, trailing: itemMargin)
-        
-        //그룹
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(200))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
-//        group.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
-        
-        //섹션
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .none
-//        section.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
-        
-        return section
+extension SearchMainViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print("updateSearchResults")
     }
 }
--- UnsplashCloneCode/ViewController/Search/SearchMain/SearchResultCollectionViewController.swift
+++ UnsplashCloneCode/ViewController/Search/SearchMain/SearchResultCollectionViewController.swift
@@ -10,81 +10,127 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class SearchResultCollectionViewController: UICollectionViewController {
-
+    var currentSearchType: SearchType = .Photos {
+        didSet {
+            fetchFirstSearch()
+        }
+    }
+    private var isSearchFetching = false
+    private var searchPageNum = 0
+    var searchText = ""
     var items: [Any] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
+        setupCollectionView()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    private func setupCollectionView() {
+        collectionView.backgroundColor = .yellow
+        
+        collectionView.register(PhotoListCollectionViewCell.self, forCellWithReuseIdentifier: "PhotoListCollectionViewCell")
     }
-    */
+}
 
-    // MARK: UICollectionViewDataSource
+extension SearchResultCollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return items.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        switch currentSearchType {
+        case .Photos:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoListCollectionViewCell", for: indexPath) as? PhotoListCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            guard let item = items[indexPath.row] as? Photo else {
+                return UICollectionViewCell()
+            }
+            cell.setup(photo: item)
+            return cell
+            
+        case .Collections:
+            return UICollectionViewCell()
+            
+        case .Users:
+            return UICollectionViewCell()
+        }
+    }
+}
+
+extension SearchResultCollectionViewController {
     
-        // Configure the cell
+    func resetResult() {
+        items = []
+        collectionView.reloadData()
+    }
     
-        return cell
+    func fetchFirstSearch(searchText: String = "") {
+        searchPageNum = 0
+        if !searchText.isEmpty {
+            self.searchText = searchText
+        }
+        fetchSearch()
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    func fetchSearch() {
+        if isSearchFetching {
+            return
+        }
+        isSearchFetching = true
+        
+        UnsplashAPI.fetchSearchResult(searchText: searchText, searchType: currentSearchType, pageNum: searchPageNum + 1) { [weak self] data, response, error in
+            self?.isSearchFetching = false
+            guard error == nil,
+                  let response = response as? HTTPURLResponse,
+                  let data = data else {
+                      DispatchQueue.main.async {    //에러 발생 시 에러 보여주기
+                          self?.showNetworkErrorAlert(error: .networkError)
+                      }
+                      return
+                  }
+            
+            switch response.statusCode {
+            //response 성공 시, 목록 설정하기
+            case (200...299):
+                do {
+//                    print("data : \(data.description)")
+                    let result = try JSONDecoder().decode(SearchPhotos.self, from: data)
+                    
+                    if self?.searchPageNum == 0 { //첫페이지를 가져온 경우 목록 설정
+                        self?.items = result.results
+                    } else { //첫페이지 외 다음페이지를 가져온 경우 목록 설정
+                        self?.items.append(contentsOf: result.results)
+                    }
+                    
+                    DispatchQueue.main.async {
+                        //다음 페이지 번호 설정
+                        self?.searchPageNum += 1
+                        self?.collectionView.layoutIfNeeded()
+                        self?.collectionView.reloadData()
+                    }
+                } catch {
+                    DispatchQueue.main.async {  //에러 발생 시 에러 보여주기
+                        self?.showNetworkErrorAlert(error: .jsonParsingError)
+                    }
+                }
+                
+            default:
+                DispatchQueue.main.async {  //에러 발생 시 에러 보여주기
+                    self?.showNetworkErrorAlert(error: .networkError)
+                }
+                return
+            }
+        }
     }
-    */
-
 }
